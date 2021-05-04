25ab6ae95e7abb9d0aeb7f5eadbf09bdbeaf9906
Rolanduwxcc <74523466+rolanduwxcc@users.noreply.github.com>
2021-05-03 21:44:07 -0500

    catch up commit

--- server/server.js
+++ server/server.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const {typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//create a new Apollow server and pass in schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+//integrate Apollo server with Express app as middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -10,5 +22,7 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    //log where to test GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
