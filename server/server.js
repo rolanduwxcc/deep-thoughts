b479bb2f5da74777eafd0983294d7d6278405715
Rolanduwxcc <74523466+rolanduwxcc@users.noreply.github.com>
2021-05-04 23:52:35 -0500

    done with some mutations

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 const {typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -10,7 +11,8 @@ const app = express();
 //create a new Apollow server and pass in schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 //integrate Apollo server with Express app as middleware
