788afcf7a338352df582448cf61c669f07ced454
Rolanduwxcc <74523466+rolanduwxcc@users.noreply.github.com>
2021-05-15 16:39:34 -0500

    ThoughtForm complete with mutations

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,9 +1,36 @@
 import React, { useState } from 'react';
 
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+
 const ThoughtForm = () => {
   const [thoughtText, setText] = useState('');
   const [characterCount, setCharacterCount] = useState(0);
 
+  const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+    update(cache, { data: { addThought } }) {
+      try {
+        // read what is currently in the cache
+        const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+        // add the newest thought to front of array
+        cache.writeQuery({
+          query: QUERY_THOUGHTS,
+          data: { thoughts: [addThought, ...thoughts] }
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      //update me object's cache, appending new thought to the end of array
+      const { me } = cache.readQuery({ query: QUERY_ME });
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...me, thoughts: [...me.thoughts, addThought ] } }
+      });
+    }
+  });
 
   const handleChange = event => {
     if (event.target.value.length <= 280) {
@@ -12,16 +39,26 @@ const ThoughtForm = () => {
     }
   }
 
-const handleSubmit = async event => {
-  event.preventDefault();
-  setText('');
-  setCharacterCount(0);
-}
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      await addThought({        //add thought to database
+        variables: { thoughtText }
+      });
+
+      setText('');            //clear form value
+      setCharacterCount(0);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div>
       <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
         Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>}
       </p>
       <form className="flex-row justify-center justify-space-between-md align-stretch" onSubmit={handleSubmit}>
         <textarea
